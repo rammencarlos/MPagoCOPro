e5db9a110c7f194e968e58dde8ca441cb2a2e3e5
Carlos <rammen.carlos@gmail.com>
2022-07-08 21:59:40 -0500

    initial commit

---
+++ MPago.Platform/Controllers/Product.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MPago.Platform.Controllers
+{
+    public class Product : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
